34a1e5b81c58505ebbc6d2c1c2a3367c519a11b9
Jill Arnold <jillarnold2913@gmail.com>
2023-01-04 13:24:52 -0500

    Added model to use with Schema

--- person.js
+++ person.js
@@ -7,7 +7,19 @@ const Schema = mongoose.Schema;
 //syntax for creating new schema
 const personSchema = new Schema({
   firstName: String,
-  lastname: String,
+  lastName: String,
   age: Number,
 });
 
+const Person = mongoose.model('Person', personSchema);
+
+let david = new Person({ firstName: 'David', lastName: 'Smith', age: 25 });
+console.log(david);
+//jilla@MyYoga MINGW64 ~/mongoose-practice (main)
+//$ node person.js
+// {
+//   firstName: 'David',
+//   lastName: 'Smith',
+//   age: 25,
+//   _id: new ObjectId("63b59520e204f5d3c10bc7db")
+// }
