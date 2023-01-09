8a296be88d1aedce4d186c2fb56be5e0e1ba2463
Jill Arnold <jillarnold2913@gmail.com>
2023-01-09 08:57:38 -0500

    update beers collection

--- person.js
+++ person.js
@@ -9,29 +9,45 @@ const Schema = mongoose.Schema;
 
 //syntax for creating new schema
 const personSchema = new Schema({
-  firstName: String,
+  id: Number,
+  firstName: { type: String, required: true },
   lastName: String,
-  age: Number,
-});
+  age: { type: Number, required: true },
+ });
 
 const Person = mongoose.model('Person', personSchema);
 
-let david = new Person({ firstName: 'David', lastName: 'Smith', age: 25 });
+let bob = new Person({ id: 1, firstName: 'Bob', lastName: 'Cohen', age: 39, updated_at: 2023-01-05, create_at: 2023-01-05 });
 
-let query = Person.find();
+// bob.save((error, person) => {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log(person);
+//   }
+// });
 
-query.exec().then(function (people) {
-  console.log(people);
-})
+Person.findById('63b6faee5e060709f45df1ec', (err, person) => {
+  if (err) throw err;
 
- 
-  // with just query, returns constructor only 
-  //with query.exec, returns a pending promise 
-  //let query = Person.find(), query.exec().then(functon (people) { console.log(people)})
-  //will return all people from the people DB
+  person.firstName = 'Jimmy';
 
-
-
-
-
+  person.save((err) => {
+    if (err) throw err;
+    else console.log('Person successfully updated!');
+  });
+});
+//$ node person.js
+// (node:15024) [MONGOOSE] DeprecationWarning: Mongoose: the `strictQuery` option will be switched back to `fa
+// lse` by default in Mongoose 7. Use `mongoose.set('strictQuery', false);` if you want to prepare for this ch
+// ange. Or use `mongoose.set('strictQuery', true);` to suppress this warning.
+// (Use `node --trace-deprecation ...` to show where the warning was created)
+// {
+//   firstName: 'Bob',
+//   lastName: 'Cohen',
+//   age: 39,
+//   _id: new ObjectId("63b71c54ba731234c73c3620"),
+//   __v: 0
+// }
+// Person successfully updated!
 
